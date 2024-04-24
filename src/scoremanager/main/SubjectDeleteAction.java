0609f7b103636b39a791d910a93f65d481d760c8
Lae-402 <e5bd81.lae@gmail.com>
2024-04-24 10:15:38 +0900

    科目削除つくりはじめ

---
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,34 @@
+package scoremanager.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Student;
+import dao.StudentDao;
+import tool.Action;
+
+public class SubjectDeleteAction  extends Action {
+
+	// オーバーライド
+	@Override
+    public void execute (HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+    	StudentDao sDao = new StudentDao();
+
+    	// 初期化
+    	String no = "";
+    	List<Student> student = new ArrayList<Student>(1);
+
+    	// 選択した学生の学生情報をstudentに
+    	no = request.getParameter("no");
+    	student.add(sDao.get(no));
+
+    	request.setAttribute( "student",student );
+
+    	request.getRequestDispatcher("student_delete.jsp").forward(request, response);
+
+    }
+}
