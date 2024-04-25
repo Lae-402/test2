5d74ac6c911e2df9c4b2c14a02d5d2b95c46af42
Lae-402 <e5bd81.lae@gmail.com>
2024-04-25 15:02:04 +0900

    SQL訂正、TestListSubjectDaoいったん完成な気がする

--- src/bean/TestListSubject.java
+++ src/bean/TestListSubject.java
@@ -10,11 +10,11 @@ public class TestListSubject implements java.io.Serializable {
     private String studentNo; // 学籍番号
     private String studentName; // 学生名
     private String classNum; // クラス番号
-    private Map<Integer, Integer> point; // 成績
+    private Map<Integer, Integer> points; // 成績
 
     // コンストラクタ
     public TestListSubject() {
-        this.point = new HashMap<>();
+        this.points = new HashMap<>();
     }
 
     // ゲッターとセッター
@@ -50,23 +50,27 @@ public class TestListSubject implements java.io.Serializable {
         this.classNum = classNum;
     }
 
+    // 全ての成績を取得する
+    public Map<Integer, Integer> getPoints() {
+        return points;
+    }
+
+    // 全ての成績を設定する
+    public void setPoints(Map<Integer, Integer> points) {
+    	// 引数で受け取ったマップを自身に格納
+        for ( Map.Entry<Integer, Integer> entry : points.entrySet() ) {
+        	putPoint( entry.getKey(), entry.getValue() );
+        }
+    }
+
     // 指定した回数の成績を取得する
     public String getPoint(int key) {
-        return point.get(key) != null ? point.get(key).toString() : null;
+        return points.get(key) != null ? points.get(key).toString() : null;
     }
 
     // 成績を追加または更新する
     public void putPoint(int key, int value) {
-        point.put(key, value);
+        this.points.put(key, value);
     }
 
-    // 全ての成績を取得する
-    public Map<Integer, Integer> getPoints() {
-        return point;
-    }
-
-    // 全ての成績を設定する
-    public void setPoints(Map<Integer, Integer> point) {
-        this.point = point;
-    }
 }
