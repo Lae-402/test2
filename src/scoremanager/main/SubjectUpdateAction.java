d6c1a964dc1a9411eae32ecf124479199758f511
'mknhm1' <'ktq2390308@stu.o-hara.ac.jp'>
2024-04-25 11:23:16 +0900

    アップデートサーブレット,jsp修正

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Student;
+import bean.School;
+import bean.Subject;
 import bean.Teacher;
-import dao.ClassNumDao;
-import dao.StudentDao;
+import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectUpdateAction extends Action {
@@ -22,25 +22,24 @@ public class SubjectUpdateAction extends Action {
         // セッションから教師情報を取得
         HttpSession session = request.getSession();
         Teacher teacher = (Teacher) session.getAttribute("user");
+        School school = teacher.getSchool();
 
         // 学生DAOとクラス番号DAOのインスタンス化
-        StudentDao sDao = new StudentDao();
-        ClassNumDao cNumDao = new ClassNumDao();
+        SubjectDao sDao = new SubjectDao();
+
 
         // 初期化
-        String studentId = ""; // 選択された学生の学籍番号
-        List<Student> students = new ArrayList<>(); // 学生情報を格納するリスト
+        String cd = ""; // 選択された学生の学籍番号
+        List<Subject> subject = new ArrayList<>(); // 学生情報を格納するリスト
 
         // リクエストパラメータから学籍番号を取得し、該当する学生情報をリストに追加
-        studentId = request.getParameter("no");
-        students.add(sDao.get(studentId)); // 学籍番号を元に学生情報を取得し、リストに追加
+        cd = request.getParameter("cd");
+        subject.add(sDao.get(cd,school)); // 科目番号を元に学生情報を取得し、リストに追加
 
-        // クラス一覧を取得
-        List<String> classList = cNumDao.filter(teacher.getSchool()); // 教師の所属する学校のクラス一覧を取得
 
         // 学生情報とクラス一覧をリクエスト属性に設定
-        request.setAttribute("student", students); // 学生情報をリクエスト属性に設定
-        request.setAttribute("class_num_set", classList); // クラス一覧をリクエスト属性に設定
+        request.setAttribute("subject", subject); // 学生情報をリクエスト属性に設定
+
 
         // 学生情報更新画面にフォワード
         request.getRequestDispatcher("subject_update.jsp").forward(request, response);
--- src/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.School;
-import bean.Student;
+import bean.Subject;
 import bean.Teacher;
-import dao.StudentDao;
+import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectUpdateExecuteAction extends Action {
@@ -22,36 +22,34 @@ public class SubjectUpdateExecuteAction extends Action {
         School school = teacher.getSchool(); // 教師の所属する学校を取得
 
         // 学生DAOのインスタンス化
-        StudentDao sDao = new StudentDao();
+        SubjectDao sDao = new SubjectDao();
+
+
+        System.out.print("test");
+
 
         // 入力値の初期化
-        int entYear = 0; // 入学年度
-        String studentNo = ""; // 学生番号
-        String name = ""; // 氏名
-        String classNum = ""; // クラス
-        boolean isAttend = false; // 在学フラグ：一律でtrue
+        String cd = ""; // 科目番号
+        String name = ""; // 科目名
         boolean done = false; // 更新が成功したかのフラグ
 
         // リクエストパラメータから入力値を取得
-        entYear = Integer.parseInt(request.getParameter("ent_year")); // 入学年度を取得
-        studentNo = request.getParameter("no"); // 学生番号を取得
+        System.out.print("test2");
+
+		cd = request.getParameter("cd"); // 学生番号を取得
         name = request.getParameter("name"); // 氏名を取得
-        classNum = request.getParameter("class_num"); // クラスを取得
-        if (request.getParameter("is_attend").equals("t")) { // 在学フラグを取得
-            isAttend = true;
-        }
+
 
         // 新しい学生オブジェクトを作成し、入力値をセット
-        Student student = new Student();
-        student.setEntYear(entYear);
-        student.setNo(studentNo);
-        student.setName(name);
-        student.setClassNum(classNum);
-        student.setAttend(isAttend);
-        student.setSchool(school); // 学校情報を設定
+        Subject subject = new Subject();
+        System.out.print("test4");
+        subject.setName(name);//科目名をセット
+        subject.setCd(cd);//科目番号をセット
+
+        subject.setSchool(school); // 学校情報を設定
 
         // 学生情報を更新し、結果を取得
-        done = sDao.save(student); // 学生情報をデータベースに保存
+        done = sDao.save(subject); // 学生情報をデータベースに保存
 
         // 更新が成功した場合は、更新完了画面にフォワード
         if (done) {
