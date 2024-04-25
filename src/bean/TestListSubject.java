39835fd10f7cff6bb416f716df32fecb7177b102
simada <ktq2390305@stu.o-hara.ac.jp>
2024-04-25 12:08:25 +0900

    TestListSubject修正（１）

--- src/bean/TestListSubject.java
+++ src/bean/TestListSubject.java
@@ -1,76 +1,72 @@
 package bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestListSubject implements java.io.Serializable {
 
     // フィールド
-    private String no;           // 科目番号
-    private String name;         // 科目名
-    private int entYear;         // 入学年度
-    private String classNum;     // クラス番号
-    private boolean isAtttend;   // 出席フラグ
-    private School school;       // 学校
+    private int entYear; // 入学年度
+    private String studentNo; // 学籍番号
+    private String studentName; // 学生名
+    private String classNum; // クラス番号
+    private Map<Integer, Integer> point; // 成績
 
-    // ゲッター
-
-    // 科目番号のゲッター
-    public String getNo() {
-        return no;
+    // コンストラクタ
+    public TestListSubject() {
+        this.point = new HashMap<>();
     }
 
-    // 科目名のゲッター
-    public String getName() {
-        return name;
-    }
-
-    // 入学年度のゲッター
+    // ゲッターとセッター
     public int getEntYear() {
         return entYear;
     }
 
-    // クラス番号のゲッター
-    public String getClassNum() {
-        return classNum;
-    }
-
-    // 出席フラグのゲッター
-    public boolean isAtttend() {
-        return isAtttend;
-    }
-
-    // 学校のゲッター
-    public School getSchool() {
-        return school;
-    }
-
-    // セッター
-
-    // 科目番号のセッター
-    public void setNo(String no) {
-        this.no = no;
-    }
-
-    // 科目名のセッター
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // 入学年度のセッター
     public void setEntYear(int entYear) {
         this.entYear = entYear;
     }
 
-    // クラス番号のセッター
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
     public void setClassNum(String classNum) {
         this.classNum = classNum;
     }
 
-    // 出席フラグのセッター
-    public void setAtttend(boolean isAtttend) {
-        this.isAtttend = isAtttend;
+    // 指定した回数の成績を取得する
+    public String getPoint(int key) {
+        return point.get(key) != null ? point.get(key).toString() : null;
     }
 
-    // 学校のセッター
-    public void setSchool(School school) {
-        this.school = school;
+    // 成績を追加または更新する
+    public void putPoint(int key, int value) {
+        point.put(key, value);
+    }
+
+    // 全ての成績を取得する
+    public Map<Integer, Integer> getPoints() {
+        return point;
+    }
+
+    // 全ての成績を設定する
+    public void setPoints(Map<Integer, Integer> point) {
+        this.point = point;
     }
 }
